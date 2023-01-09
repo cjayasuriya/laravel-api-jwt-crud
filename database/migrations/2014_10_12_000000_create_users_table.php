c3825d86fe3d5a7932d3b33e7e80c8a65d1aa963
Chamal Jayasuriya <chamaljaya@gmail.com>
2023-01-10 00:35:28 +0530

    Updating users table columns

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,17 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('firstName');
+            $table->string('middleName')->nullable();
+            $table->string('lastName');
+            $table->string('mobile')->nullable();
+            $table->text('imageP')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->text('meta')->nullable();
+            $table->integer('statusID')->default(1);
+            $table->string('status')->default('Active');
             $table->rememberToken();
             $table->timestamps();
         });
