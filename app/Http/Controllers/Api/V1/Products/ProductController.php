17f6ac805640aff4824a4d38bc42eaffb519cc5c
Chamal Jayasuriya <chamaljaya@gmail.com>
2023-01-10 12:51:08 +0530

    Updating product create and edit

--- app/Http/Controllers/Api/V1/Products/ProductController.php
+++ app/Http/Controllers/Api/V1/Products/ProductController.php
@@ -42,6 +42,76 @@ class ProductController extends Controller
      */
     public function create(Request $request){
 
+        $auid = $request->user()->id;
+
+        $prices = array(
+            "unit" => !empty($request->unitPrice) ? (double)$request->unitPrice : 0,
+            "selling" => !empty($request->sellingPrice) ? (double)$request->sellingPrice : 0,
+            "lowest" => !empty($request->lowestPrice) ? (double)$request->lowestPrice : 0,
+        );
+
+        $sizes = array(
+            "length" => !empty($request->length) ? (double)$request->length : 0,
+            "width" => !empty($request->width) ? (double)$request->width : 0,
+            "height" => !empty($request->height) ? (double)$request->height : 0,
+        );
+
+        $product = new Product();
+        $product->sku = $request->sku;
+        $product->name = $request->brand;
+        $product->model = !empty($request->model) ? $request->model : '';
+        $product->uom = !empty($request->uom) ? $request->uom : '';
+        $product->prices = json_encode($prices);
+        $product->weight = !empty($request->weight) ? (double)$request->weight : 0;
+        $product->weightUnit = !empty($request->weightUnit) ? $request->weightUnit : '';
+        $product->sizes = json_encode($sizes);
+        $product->sizeUnit = !empty($request->sizeUnit) ? $request->sizeUnit : '';
+        $product->description = !empty($request->description) ? $request->description : '';
+        $product->cuid = $auid;
+        $product->uuid = $auid;
+        $product->save();
+
+        return response()->json($product, 200);
+
+    }
+
+
+    /**
+     * Edit
+     */
+    public function edit(Request $request){
+
+        $auid = $request->user()->id;
+
+        $product = Product::findorfail($request->productID);
+
+        $prices = array(
+            "unit" => !empty($request->unitPrice) ? (double)$request->unitPrice : 0,
+            "selling" => !empty($request->sellingPrice) ? (double)$request->sellingPrice : 0,
+            "lowest" => !empty($request->lowestPrice) ? (double)$request->lowestPrice : 0,
+        );
+
+        $sizes = array(
+            "length" => !empty($request->length) ? (double)$request->length : 0,
+            "width" => !empty($request->width) ? (double)$request->width : 0,
+            "height" => !empty($request->height) ? (double)$request->height : 0,
+        );
+
+        $product->sku = $request->sku;
+        $product->name = $request->brand;
+        $product->model = !empty($request->model) ? $request->model : '';
+        $product->uom = !empty($request->uom) ? $request->uom : '';
+        $product->prices = json_encode($prices);
+        $product->weight = !empty($request->weight) ? (double)$request->weight : 0;
+        $product->weightUnit = !empty($request->weightUnit) ? $request->weightUnit : '';
+        $product->sizes = json_encode($sizes);
+        $product->sizeUnit = !empty($request->sizeUnit) ? $request->sizeUnit : '';
+        $product->description = !empty($request->description) ? $request->description : '';
+        $product->uuid = $auid;
+        $product->update();
+
+        return response()->json($product, 200);
+
     }
 
 }
--- routes/api.php
+++ routes/api.php
@@ -26,3 +26,12 @@ Route::post('/v1/auth/register', [App\Http\Controllers\Api\V1\Auth\AuthControlle
 Route::post('/v1/auth/login', [App\Http\Controllers\Api\V1\Auth\AuthController::class, 'login']);
 Route::post('/v1/auth/logout', [App\Http\Controllers\Api\V1\Auth\AuthController::class, 'logout']);
 Route::post('/v1/auth/profile', [App\Http\Controllers\Api\V1\Auth\AuthController::class, 'profile']);
+
+
+/**
+ * Products
+ */
+Route::get('/v1/products/', [App\Http\Controllers\Api\V1\Products\ProductController::class, 'index']);
+Route::get('/v1/products/create/save', [App\Http\Controllers\Api\V1\Products\ProductController::class, 'create']);
+Route::get('/v1/products/{productID}', [App\Http\Controllers\Api\V1\Products\ProductController::class, 'show']);
+Route::get('/v1/products/{productID}/edit/update', [App\Http\Controllers\Api\V1\Products\ProductController::class, 'edit']);
